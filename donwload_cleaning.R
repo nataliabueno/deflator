ad8a996c67b26f8ad7a302135e4ffae3be153472
Natalia Bueno <natalia.bueno@yale.edu>
2017-03-03 22:40:42 -0300

    cleaning

--- donwload_cleaning.R
+++ donwload_cleaning.R
@@ -3,7 +3,9 @@
 # Cleaning and organizing
 ##########################
 
+library(readxl)
 library(tidyverse)
+library(stringr)
 
 #Downloading and unzipping
 download.file(url = "ftp://ftp.ibge.gov.br/Precos_Indices_de_Precos_ao_Consumidor/IPCA/Serie_Historica/ipca_SerieHist.zip",
@@ -14,4 +16,25 @@ unzip(zipfile="~/Dropbox/deflator/ipca_serie.zip",
 
 #Cleaning
 
-readxl("~/Dropbox/deflator/ipca_201701SerieHist.xls")
+ipca <- read_excel("~/Dropbox/deflator/ipca_201701SerieHist.xls")
+ipca <- ipca[-c(1:6),]
+names(ipca) <- c("ano", "mes", "numero_indice", "no_mes", "tres_meses", 
+                 "seis_meses", "no_ano", "doze_meses")
+ipca$ano <- gsub("Fonte: IBGE, Diretoria de Pesquisas, Coordenação de Índices de Preços, ", 
+                 NA, ipca$ano)
+ipca$ano <- gsub("SÉRIE HISTÓRICA DO IPCA", 
+                 NA, ipca$ano)
+ipca$ano <- gsub("ANO", 
+                 NA, ipca$ano)
+ipca$ano <- gsub("Sistema Nacional de Índices de Preços ao Consumidor.", 
+                 NA, ipca$ano)
+ipca <- ipca %>% filter(mes != "MÊS") %>% 
+        mutate(ano = as.numeric(ano),
+               numero_indice = as.numeric(numero_indice),
+               no_mes = as.numeric(no_mes),
+               tres_meses = as.numeric(tres_meses),
+               seis_meses = as.numeric(seis_meses),
+               no_ano = as.numeric(no_ano),
+               doze_meses = as.numeric(doze_meses))
+
+#Pick month and year for reference
